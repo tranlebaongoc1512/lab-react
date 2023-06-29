aadb184d647fed3ae4fe9e288255bf9ed09b9130
tranlebaongoc1512 <tranlebaongoc1512@gmail.com>
2023-06-29 20:43:05 +0700

    fix

--- src/components/hooks/useAuth.js
+++ src/components/hooks/useAuth.js
@@ -2,6 +2,7 @@ import { useContext } from "react"
 import { AuthContext } from "../../context/AuthContext"
 
 const useAuth = () => {
+    console.log(useContext(AuthContext))
     return useContext(AuthContext)
 }
 export default useAuth;
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import filmSlice from "./features/filmSligce";
+import filmSlice from "./features/filmSlice";
 export const store = configureStore({
     reducer: {
         film: filmSlice.reducer,
