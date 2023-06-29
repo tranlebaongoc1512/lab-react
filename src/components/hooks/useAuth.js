a08919d6ae7e589489c362f7ba6272a4f46caa69
tranlebaongoc1512 <tranlebaongoc1512@gmail.com>
2023-06-29 23:58:33 +0700

    fix

--- src/components/hooks/useAuth.js
+++ src/components/hooks/useAuth.js
@@ -2,7 +2,6 @@ import { useContext } from "react"
 import { AuthContext } from "../../context/AuthContext"
 
 const useAuth = () => {
-    console.log(useContext(AuthContext))
     return useContext(AuthContext)
 }
 export default useAuth;
